8bc960191d459f745f8a216e195ef2d6bc08ec54
ERIC <331090189@qq.com>
2020-11-13 10:20:04 +0800

    begin

---
+++ clounds/clound-config/src/main/java/com/eric/demo/clound/config/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.eric.demo.clound.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author wuwei
+ * @createTime 20201112 16:15
+ * @description 启动类
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
